0e413d52a47a1df94ae07d105ba437936ae37ed2
Nathan Kluth <nlkluth@gmail.com>
2014-01-18 12:19:37 -0700

    She jumps!

--- Cepholescent/Assets/Scripts/PlayerMovement.cs
+++ Cepholescent/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,13 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour 
 {
 	public float maxSpeed = 10f;
+	public Transform groundCheck;
+	public LayerMask groundLayerMask;
+	public float jumpForce = 30f;
+
 	private bool facingRight = true;
+	private bool grounded = false;
+	private float groundRadius = 0.2f;
 	private Animator animator;
 
 	void Start() 
@@ -14,12 +20,31 @@ public class PlayerMovement : MonoBehaviour
 
 	void FixedUpdate() 
 	{
+		CheckIfGrounded();
+
 		float move = Input.GetAxis("Horizontal");
 		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
 		animator.SetFloat("Speed", Mathf.Abs(move));
 
 		if (move > 0 && !facingRight) Flip ();
 		else if (move < 0 && facingRight) Flip ();
+	}
+
+	void Update()
+	{
+		if (grounded && (Input.GetAxis("Jump") > 0))
+		{
+			animator.SetBool("Ground", false);
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+		}
+	}
+
+	private void CheckIfGrounded()
+	{
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayerMask);
+		animator.SetBool("Ground", grounded);
+
+		animator.SetFloat("VerticalSpeed", rigidbody2D.velocity.y);
 	}
 
 	private void Flip()
