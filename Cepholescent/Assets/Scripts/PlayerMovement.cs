5647a589beeac47ec2c02ebdfd59ea753ffb80d0
Nathan Kluth <nlkluth@gmail.com>
2014-01-18 11:43:24 -0700

    starting player movement

--- Cepholescent/Assets/Scripts/PlayerMovement.cs
+++ Cepholescent/Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour 
+{
+	public float maxSpeed = 10f;
+	private bool facingRight = true;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start() {}
+
+	void FixedUpdate() 
+	{
+		float move = Input.GetAxis("Horizontal");
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+		if (move > 0 && !facingRight) Flip ();
+		else if (move < 0 && facingRight) Flip ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 updatedPlayerScale = transform.localScale;
+		updatedPlayerScale.x *= -1;
+		transform.localScale = updatedPlayerScale;
 	}
 }
