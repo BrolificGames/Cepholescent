5837a2cf15cd7af953cca6268ff3aaea29a8c60e
Nathan Kluth <nlkluth@gmail.com>
2014-01-18 11:49:39 -0700

    She runs!

--- Cepholescent/Assets/Scripts/PlayerMovement.cs
+++ Cepholescent/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,18 @@ public class PlayerMovement : MonoBehaviour
 {
 	public float maxSpeed = 10f;
 	private bool facingRight = true;
+	private Animator animator;
 
-	void Start() {}
+	void Start() 
+	{
+		animator = GetComponent<Animator>();
+	}
 
 	void FixedUpdate() 
 	{
 		float move = Input.GetAxis("Horizontal");
 		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+		animator.SetFloat("Speed", Mathf.Abs(move));
 
 		if (move > 0 && !facingRight) Flip ();
 		else if (move < 0 && facingRight) Flip ();
