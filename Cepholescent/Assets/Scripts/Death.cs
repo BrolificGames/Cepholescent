ca5e5ba02448f646ef60f1dab3d200f317e078b7
Nathan Kluth <nlkluth@gmail.com>
2014-03-04 12:40:54 -0700

    death area moves along underneath player

--- Cepholescent/Assets/Scripts/Death.cs
+++ Cepholescent/Assets/Scripts/Death.cs
@@ -4,6 +4,24 @@ using System.Collections;
 public class Death : MonoBehaviour 
 {
 	public GameObject Fader;
+	private GameObject player;
+	private float positionX;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void FixedUpdate()
+	{
+		TrackPlayer();
+	}
+
+	private void TrackPlayer()
+	{
+		positionX = player.transform.position.x;
+		transform.position = new Vector3(positionX, -7.8f, transform.position.z);
+	}
 
 	void OnTriggerEnter2D(Collider2D collision) {
 		if (collision.gameObject.tag == "Player")
