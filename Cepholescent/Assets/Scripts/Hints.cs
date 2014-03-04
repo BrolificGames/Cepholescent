00e9c1878ddcb7dbbbdeed447201b9032f8164e9
Nathan Kluth <nlkluth@gmail.com>
2014-03-04 12:41:09 -0700

    added hints script

---
+++ Cepholescent/Assets/Scripts/Hints.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hints : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.gameObject.tag == "Player")
+		{
+			Debug.Log ("show hint");
+		}
+	}
+}
