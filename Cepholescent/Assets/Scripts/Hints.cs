95262b76ecd402b6d3c1c16f3c4a8a3ced19ef5a
Nathan Kluth <nlkluth@gmail.com>
2014-03-05 11:24:34 -0700

    wait for keypress to close dialog

--- Cepholescent/Assets/Scripts/Hints.cs
+++ Cepholescent/Assets/Scripts/Hints.cs
@@ -3,12 +3,36 @@ using System.Collections;
 
 public class Hints : MonoBehaviour {
 	public bool shown = false;
+	private bool _keyPressed = false;
 
 	void OnTriggerEnter2D(Collider2D collision) {
 		if (collision.gameObject.tag == "Player" && shown == false)
 		{
 			Debug.Log ("showing hint");
 			shown = true;
+			StartCoroutine(showHint());
 		}
+	}
+
+	private IEnumerator showHint()
+	{
+		openDialog();
+
+		while (!Input.GetKeyUp("space"))
+		{
+			yield return null;
+		}
+
+		closeDialog();
+	}
+
+	private void openDialog()
+	{
+		Debug.Log ("open dialog);
+	}
+
+	private void closeDialog()
+	{
+		Debug.Log ("hide hint");
 	}
 }
