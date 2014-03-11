1898dc4a2908d12ddb3de5d55eb59e36de552362
Nathan Kluth <nlkluth@gmail.com>
2014-03-11 15:02:04 -0600

    bubbles follow player

---
+++ Cepholescent/Assets/Scripts/BubblesFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubblesFollow : MonoBehaviour 
+{
+	private float positionX;
+	private float positionY;
+	private Transform player;
+	
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform.Find("BubbleSpawn");
+	}
+	
+	void FixedUpdate()
+	{
+		TrackPlayer();
+	}
+	
+	private void TrackPlayer()
+	{
+		positionX = player.position.x;
+		positionY = player.position.y;
+		
+		transform.position = new Vector3(positionX, positionY, transform.position.z);
+	}
+}
