cd3c86a7b77ee2f55d53334048754db799feafe3
Nathan Kluth <nlkluth@gmail.com>
2014-03-15 10:58:36 -0600

    setparticlesystem abstracted to allow future use

---
+++ Cepholescent/Assets/Scripts/SetParticleSortingLayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetParticleSortingLayer : MonoBehaviour 
+{
+	public string Layer;
+	public int layerNumber;
+
+	void Start()
+	{
+		// Set the sorting layer of the particle system.
+		particleSystem.renderer.sortingLayerName = Layer;
+		particleSystem.renderer.sortingOrder = layerNumber;
+	}
+}
